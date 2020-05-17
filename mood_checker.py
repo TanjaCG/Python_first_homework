53059666d0ffd8968fcbf1b9a4a3261b2168e3b8
Tanja CukG <tanja.cg8@gmail.com>
2020-05-17 19:35:07 +0200

    add mood_checker.py

---
+++ mood_checker.py
@@ -0,0 +1,15 @@
+mood = input("What is your mood today?")
+
+if mood == "happy":
+    print("It is great to see you happy!")
+elif mood == "nervous":
+    print("Take a deep breath 3 times.")
+elif mood == "sad":
+    print("Go get an ice cream.")
+elif mood == "excited":
+    print("Bring it on!")
+elif mood == ("relaxed"):
+    print("Just chill..")
+else:
+    print("I don't recognize this mood")
+
