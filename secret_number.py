f447ab9c0f11a3de9377417398b33b3128c3dd69
Tanja CukG <tanja.cg8@gmail.com>
2020-05-17 19:24:26 +0200

    add calculator.py

--- secret_number.py
+++ secret_number.py
@@ -1,1 +1,8 @@
+secret = 35
 
+guess = int(input("Guess the secret number (between 0 and 45): "))
+
+if guess == secret:
+    print("Congratulations! The secret number is 35.")
+else:
+    print("Sorry, your guess is not correct. Try again another time.")
